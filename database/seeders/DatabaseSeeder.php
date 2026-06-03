5bbb9c7ab7fd4a2c078620b39619159ee481e5f3
Ilham Ghazali <m.ilhamghazali@gmail.com>
2026-06-03 14:01:54 +0700

    feat: initialize Filament admin panel with RBAC, resources for Guru, Siswa, and Nilai, and custom reporting pages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test Ilham ',
+            'email' => 'ilham@admin.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test user ',
+            'email' => 'user@user.com',
+            'password' => Hash::make('45678912'),
         ]);
     }
 }
